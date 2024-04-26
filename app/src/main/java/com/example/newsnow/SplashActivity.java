81f1e276719f4bd2a739f9b75dc983c423fcbc8f
Manasasonu123 <manasasonukulal@gmail.com>
2024-04-26 23:51:20 +0530

    Trying to commit

--- app/src/main/java/com/example/newsnow/SplashActivity.java
+++ app/src/main/java/com/example/newsnow/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        System.out.println("hi");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
